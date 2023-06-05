435220504fe9f2b1289c6d542164259ec2322ef7
ProgramNS <blise01@hotmail.com>
2023-06-05 17:48:57 +0700

    Update users.js

--- controller/users.js
+++ controller/users.js
@@ -1,21 +1,23 @@
+require('dotenv').config()
 const express = require('express');
 const routes = express.Router();
 const db = require('../database/connection');
 const { uuid } = require('short-uuid');
 const bcrypt = require('bcrypt');
 const uid = uuid()
+const jwt = require('jsonwebtoken');
 
 const createUsers = routes.post('/register' , async (req , res)=>{
     const {name , email} = req.body;
-    const sql = "INSERT INTO users (user_id , name , email , password) VALUES(?,?,?,?)";
-    const hashPassword = await bcrypt.hash(req.body.password, 10);
+    const sql = "INSERT INTO user (user_id , name , email , password) VALUES(?,?,?,?)";
+    const hashPassword =  await bcrypt.hash(req.body.password, 10);
     db.query(sql , [uid , name , email , hashPassword] ,(err)=>{
         res.setHeader('Content-Type','application/json');
         if(err) return res.status(403).json({
             error : true,
             message : "Failed to create account"
         });
-        res.status(200).json({
+        res.status(201).json({
             error : false,
             message : "Created account success"
         })
@@ -24,7 +26,29 @@ const createUsers = routes.post('/register' , async (req , res)=>{
 });
 
 const loginUsers = routes.post('/login' , (req, res)=>{
-    
-})
+    const {email , password} = req.body;
+    const sql = "SELECT * FROM user WHERE email =?";
+    const accessToken = jwt.sign({email : email}, process.env.ACCESS_TOKEN_SECRET);
+    db.query(sql , [email] , (err , fields)=>{
+        if(err) throw err;
+        if (fields.length === 0 ) {
+            res.status(404).json({
+                error : true,
+                message : "Email not found"
+            });
+        } else {
+            res.status(200).json({
+                error : false,
+                message : "success",
+                loginResult : {
+                    user_id  : fields[0].user_id,
+                    name : fields[0].name,
+                    email : fields[0].email,
+                    token : accessToken
+                }
+            });
+        }
+    });
+});
 
-module.exports = {createUsers};
+module.exports = {createUsers , loginUsers};
