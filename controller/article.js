5e8f51fd7cbc5a18ccb6ce3d60c13d5a85c024c9
ProgramNS <blise01@hotmail.com>
2023-06-05 17:48:50 +0700

    Update Routes ID

--- controller/article.js
+++ controller/article.js
@@ -3,55 +3,55 @@ const routes = express.Router();
 const db = require('../database/connection');
 const { v4: uuidv4 } = require('uuid');
 
-const routesArticles = routes.get('/articles' , (req , res)=>{
-    const sql = "SELECT * FROM articles1";
+const routesArticles = routes.get('/article' , (req , res)=>{
+    const sql = "SELECT * FROM article";
     db.query(sql , (err , fields)=>{
         if (err) throw err;
         res.status(200).json({
-            data : fields
+            data : fields ,
         });
     });
 });
 
-const getRouteById = routes.get('/articles/:article_id' , (req , res) =>{
-    const sql = "SELECT article_id , judul , penjelasan , link FROM articles1 WHERE article_id = ?";
+const getRouteById = routes.get('/article/:article_id' , (req , res) =>{
+    res.setHeader('Content-Type','aplication/json');
     const id = req.params.article_id;
+    const sql = `SELECT article_id , judul , penjelasan , link FROM article WHERE article_id = ?`;
     db.query(sql , [id] , (err , fields)=>{
-        if(err) {
-            res.setHeader('Content-Type','application/json');
+        if(fields.length === 0) {
             res.status(404).json({
                 error : true,
-                message : "Data Not Found"
-            });
+                message : "Article not found"
+            })
         } else {
-            res.setHeader('Content-Type','application/json');
             res.status(200).json({
                 error : false,
-                message : "Data Ditemukan",
-                data : [
+                message : "Get Article By ID",
+                data : 
+                [
                     {
                         article : fields
                     }
                 ]
-            })
+            });
         }
-    })
-})
+    });
+});
 
-const postRouteArticles = routes.post('/articles' , (req , res)=>{
-    const sql = "INSERT INTO articles1 (article_id , judul , penjelasan , link) VALUES(?,?,?,?)";
+const postRouteArticles = routes.post('/article' , (req , res)=>{
+    const sql = "INSERT INTO article (article_id , judul , penjelasan , link) VALUES(?,?,?,?)";
     const {judul , penjelasan , link} = req.body;
     const articleId = uuidv4();
     db.query(sql , [articleId, judul , penjelasan , link] , (err)=>{
         if (err) {
-            res.setHeader('Content-Type','application/json');
+            
             res.status(400).json({
                 erorr : true,
                 message : 'Gagal menambahkan article'
             });
             console.log({judul , penjelasan , link});
         } else{
-            res.setHeader('Content-Type','application/json');
+            
             res.status(200).json({
                 error : false,
                 message : 'Berhasil menambahkan article'
